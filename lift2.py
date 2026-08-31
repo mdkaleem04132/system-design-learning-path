6f2308f5097b45482fd7d0cbf84dcb48ead85893
Md kaleemulla <mdkaleem01432@gmail.com>
2026-08-31 19:08:34 +0530

    Update lift2.py

--- lift2.py
+++ lift2.py
@@ -1,7 +1,4 @@
-
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
+None):
 #         self.val = val
 #         self.next = next
 class Solution(object):
